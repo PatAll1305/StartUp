6b3f3af220c3b32084bea011b26e3d82ef066860
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-11 17:05:51 -0600

    finished rewards seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,8 @@ from flask.cli import AppGroup
 from ..models import environment
 from .users import seed_users, undo_users
 from .projects import seed_projects, undo_projects
+from .rewards import seed_rewards, undo_rewards
+from .categories import seed_categories, undo_categories
 
 seed_commands = AppGroup('seed')
 
@@ -11,12 +13,18 @@ def seed():
     if environment == 'production':
         undo_projects()
         undo_users()
-    
-    seed_users()     
-    seed_projects()  
+        undo_rewards()
+        undo_categories()
+
+    seed_users()
+    seed_projects()
+    seed_rewards()
+    seed_categories()
 
 @seed_commands.command('undo')
 def undo():
     """Undo all seeded data."""
     undo_projects()
     undo_users()
+    undo_rewards()
+    undo_categories()
