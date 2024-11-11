e4e1768fc28eef89beb63da670b0d4564cde9acc
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-11 17:23:14 -0600

    added both backedproj my changes to seeds/__init__.py

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,9 +2,8 @@ from flask.cli import AppGroup
 from ..models import environment
 from .users import seed_users, undo_users
 from .projects import seed_projects, undo_projects
-from .rewards import seed_rewards, undo_rewards
-from .categories import seed_categories, undo_categories
 from .backedProjects import seed_backedProjects, undo_backedProjects
+from .categories import seed_categories, undo_categories
 
 seed_commands = AppGroup('seed')
 
@@ -12,16 +11,20 @@ seed_commands = AppGroup('seed')
 def seed():
     """Seed all data."""
     if environment == 'production':
-        undo_backedProjects
+        undo_categories()
+        undo_backedProjects()
         undo_projects()
         undo_users()
 
     seed_users()
     seed_projects()
+    seed_backedProjects()
+    seed_categories()
 
 @seed_commands.command('undo')
 def undo():
     """Undo all seeded data."""
-    undo_backedProjects
+    undo_categories()
+    undo_backedProjects()
     undo_projects()
     undo_users()
