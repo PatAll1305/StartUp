f6ec686181678a658b6b47b13005116ba2191f56
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-11 22:12:59 -0500

    deploy

--- app/models/backedProjects.py
+++ app/models/backedProjects.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA
+from sqlalchemy.sql import text
 
 class BackedProject(db.Model):
   __tablename__ = 'backed_projects'
@@ -7,9 +8,20 @@ class BackedProject(db.Model):
     __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-  project_id = db.Column(db.Integer, db.ForeignKey('projects.id', ondelete='CASCADE'), nullable=False)
-  reward_id = db.Column(db.Integer, db.ForeignKey('rewards.id', ondelete='SET NULL'))
+  user_id = db.Column(
+    db.Integer, 
+    db.ForeignKey(f'{SCHEMA}.users.id' if environment == "production" else 'users.id', ondelete='CASCADE'), 
+    nullable=False
+  )
+  project_id = db.Column(
+    db.Integer, 
+    db.ForeignKey(f'{SCHEMA}.projects.id' if environment == "production" else 'projects.id', ondelete='CASCADE'), 
+    nullable=False
+  )
+  reward_id = db.Column(
+    db.Integer, 
+    db.ForeignKey(f'{SCHEMA}.rewards.id' if environment == "production" else 'rewards.id', ondelete='SET NULL')
+  )
 
   user = db.relationship('User', back_populates='backed_projects')
   project = db.relationship('Project', back_populates='backed_projects')
