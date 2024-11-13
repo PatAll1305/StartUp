e4b8fc63b3ecc67abbbf85ad6d6ebc938e5082ae
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-13 14:54:21 -0500

    try login

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -33,37 +33,47 @@ function LoginFormPage() {
 
   return (
     <>
-    <ul className="navlist-login">
-      <li className="login-startup">
-        <NavLink to="/">StartUp</NavLink>
-      </li>
-    </ul>
-      <h1>Log In</h1>
+      <ul className="navlist-login-page">
+        <li className="login-startup-title">
+          <NavLink to="/">StartUp</NavLink>
+        </li>
+      </ul>
+      <h1 className="login-form-heading">Log In</h1>
       {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <label>
+        errors.map((message) => <p key={message} className="error-message">{message}</p>)}
+      <form onSubmit={handleSubmit} className="login-form">
+        <label htmlFor="email" className="login-label">
           Email
           <input
             type="text"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            className="login-input"
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
+        {errors.email && <p className="error-message">{errors.email}</p>}
+        <label htmlFor="password" className="login-label">
           Password
           <input
             type="password"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            className="login-input"
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
+        {errors.password && <p className="error-message">{errors.password}</p>}
+        <button type="submit" className="login-submit-button">Log In</button>
       </form>
+      <div className="signup-section">
+        <hr className="divider" />
+        <p className="signup-text">
+          New to the site? <NavLink to="/signup" className="signup-link">Sign Up</NavLink>
+        </p>
+      </div>
     </>
   );
 }
