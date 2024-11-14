2c57f1b51014f0919da7dc1964d7cb12fc546a51
Patrick Allen <patrickallen1305@gmail.com>
2024-11-14 15:41:59 -0600

    Fixed relationship issue with backed_projects not relating to users

--- app/models/backedProjects.py
+++ app/models/backedProjects.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 from .db import db, environment, SCHEMA
-from sqlalchemy.sql import text
 
 class BackedProject(db.Model):
   __tablename__ = 'backed_projects'
@@ -8,17 +8,13 @@ class BackedProject(db.Model):
     __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  user_id = db.Column(
-    db.Integer, 
-    db.ForeignKey(f'{SCHEMA}.users.id' if environment == "production" else 'users.id', ondelete='CASCADE'), 
-    nullable=False
-  )
-  project_id = db.Column(
-      db.Integer, 
-      db.ForeignKey(f'{SCHEMA}.projects.id' if environment == "production" else 'projects.id', ondelete='CASCADE'), 
-      nullable=False
-  )
-  reward_id = db.Column(db.Integer)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+  project_id = db.Column(db.Integer, db.ForeignKey('projects.id', ondelete='CASCADE'), nullable=False)
+  reward_id = db.Column(db.Integer, db.ForeignKey('rewards.id', ondelete='SET NULL'))
+
+  user = db.relationship('User', back_populates='backed_projects')
+  project = db.relationship('Project', back_populates='backed_projects')
+  reward = db.relationship('Reward', back_populates='backed_projects')
 
   def to_dict(self):
     return {
