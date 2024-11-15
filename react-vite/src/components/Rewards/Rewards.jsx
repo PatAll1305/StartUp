a793db63c2e2c23b56e8b6b16e591c29d0adace5
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-15 10:30:25 -0600

    adding styling to rewards

--- react-vite/src/components/Rewards/Rewards.jsx
+++ react-vite/src/components/Rewards/Rewards.jsx
@@ -10,7 +10,7 @@ const Rewards = () => {
     const dispatch = useDispatch();
     const rewards = useSelector((state) =>
         Object.values(state.rewards).filter((reward) => reward.project_id === Number(projectId)))
-    
+
     useEffect(() => {
         dispatch(getRewardsThunk())
     }, [dispatch])
@@ -18,12 +18,25 @@ const Rewards = () => {
     return (
         <div className="each-reward">
             {rewards.length > 0 ? (
-                rewards.map((reward) => (
+                <>
+                {rewards.map((reward) => (
                 <div className="rewards-content" key={reward.id}>
                     <h4 className="rewards-desc">{reward.content}</h4>
                     <button>Pledge: ${reward.pledge}</button>
                 </div>
-            ))
+            ))}
+            <div
+                className="rewards-content"
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}>
+                <input type="number" placeholder="Choose your donation amount"/>
+                <button>Donate</button>
+            </div>
+
+                </>
         ) : (
             <h1>No rewards available</h1>
         )}
